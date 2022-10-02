0ec8b2c4ad3a6b213dfaa3abbee38a4d3a26c55a
Kieffer <kganza@traxiontech.net>
2022-10-02 09:49:59 +0800

    feature: added active status on navbar status

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded border">
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 border">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="https://flowbite.com/" class="flex items-center">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
@@ -29,32 +29,32 @@
           >
             <div class="py-1" role="none">
               <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-              <a id="menu-item-0" href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1">Account settings</a>
-              <button type="button"
-                      class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem"
-                      tabindex="-1"
-                      @click.prevent="logout"
-              >
-                Sign out
-              </button>
+              <a id="menu-item-0" href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">
+                <router-link :to="{ name: 'settings.profile' }" class="nav-link">
+                  Account Settings
+                </router-link>
+              </a>
+              <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" @click.prevent="logout">
+                Sign Out
+              </a>
             </div>
           </div>
         </div>
       </div>
       <template v-else>
         <div id="navbar-default" class="hidden w-full md:block md:w-auto">
-          <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li>
-              <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 bg-blue-700 rounded md:bg-transparent md:p-0" :class="{'text-blue-700': routePath === 'login'}">
                 <router-link :to="{ name: 'login' }" class="nav-link" active-class="active">
-                  {{ $t('login') }}
+                  Log In
                 </router-link>
               </a>
             </li>
             <li>
-              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 bg-blue-700 rounded md:bg-transparent md:p-0" :class="{'text-blue-700': routePath === 'register'}">
                 <router-link :to="{ name: 'register' }" class="nav-link" active-class="active">
-                  {{ $t('register') }}
+                  Register
                 </router-link>
               </a>
             </li>
@@ -74,9 +74,13 @@ export default {
       showDropdown: false
     }
   },
-  computed: mapGetters({
-    user: 'auth/user'
-  }),
+
+  computed: {
+    ...mapGetters('auth', ['user']),
+    routePath() {
+      return this.$route.name
+    }
+  },
 
   methods: {
     async logout () {
